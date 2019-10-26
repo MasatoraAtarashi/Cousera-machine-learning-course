7ea4188262ee5c696b84c93325e5244bda382db7
あたらしまさとら <masatora.atarashi@gmail.com>
2019-10-27 02:20:01 +0900

    Finish week8 tasks

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -1,7 +1,7 @@
 function centroids = computeCentroids(X, idx, K)
-%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%COMPUTECENTROIDS returns the new centroids by computing the means of the
 %data points assigned to each centroid.
-%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
 %   computing the means of the data points assigned to each centroid. It is
 %   given a dataset X where each row is a single data point, a vector
 %   idx of centroid assignments (i.e. each entry in range [1..K]) for each
@@ -26,8 +26,19 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
+%ループなしの実装
+labels = [repmat(idx(:),n,1) kron(1:n,ones(1,numel(idx))).'];
+totals = accumarray(labels,X(:));
+count_of_x_each_K = accumarray(idx,1);
+centroids = totals ./ count_of_x_each_K;
 
-
+%for文を使ったやつ。
+%count_of_x_each_K = zeros(K,1);
+%for i = 1:m
+%  centroids(idx(i),:) = centroids(idx(i),:) + X(i,:);
+%  count_of_x_each_K(idx(i)) = count_of_x_each_K(idx(i)) + 1;
+%end
+%centroids = centroids ./ count_of_x_each_K;
 
 
 
@@ -37,4 +48,3 @@ centroids = zeros(K, n);
 
 
 end
-
